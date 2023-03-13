bb06feb88b20b3031e6caa29b845d15fbdf04e36
user <user@XVIII-DEMS.local>
2023-03-13 23:18:13 +0100

    added logout button

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useServiceProviderValue } from "../ServiceProvider";
 import "./Styles/Section.css";
 
 function Section() {
+  const [{ token }, dispatch] = useServiceProviderValue();
+  const logout = () => {
+    dispatch({
+      type: "SET_TOKEN",
+      token: "",
+    });
+  };
   return (
     <div className="section">
-      <h2>Shortcuts</h2>
+      <button className="logout_button" onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 }
