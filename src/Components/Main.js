8d10121e417db7e759070f64f86929ff66081c04
user <user@XVIII-DEMS.local>
2023-03-15 08:15:00 +0100

    another commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -12,32 +12,20 @@ function Main() {
   const [{ token, playlists, user }] = useServiceProviderValue();
   //const id = playlists.items[0].id;
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="main">
-              <div className="search_wrapper">
-                {/*<SearchIcon className="search_icon" />*/}
-                <input
-                  type="text"
-                  placeholder="Search for artist,songs and ..."
-                ></input>
-              </div>
+    <div className="main">
+      <div className="search_wrapper">
+        {/*<SearchIcon className="search_icon" />*/}
+        <input
+          type="text"
+          placeholder="Search for artist,songs and ..."
+        ></input>
+      </div>
 
-              <p>What's hot🔥</p>
+      <p>What's hot🔥</p>
 
-              <Trending />
-              <PlayList />
-            </div>
-          }
-        />
-        <Route path="allplaylists" element={<AllPlayLists />} />
-
-        <Route path="playlisttracks" element={<PlayListTracks />} />
-      </Routes>
-    </BrowserRouter>
+      <Trending />
+      <PlayList />
+    </div>
   );
 }
 
